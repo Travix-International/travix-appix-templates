f0641a3dffb433e99dc6326406431decb1d45e89
Fahad Ibnay Heylaal <fahad19@gmail.com>
2017-11-17 11:45:41 +0100

    log errors.

--- css-modules/ui/helloWorldWidget/components/Index.js
+++ css-modules/ui/helloWorldWidget/components/Index.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import { observe } from 'frint-react';
 
 import Root from './Root';
 
-export default class Index extends React.Component {
+class Index extends React.Component {
   constructor(props) {
     super(props);
 
@@ -12,18 +13,37 @@ export default class Index extends React.Component {
   }
 
   componentDidCatch(error, info) {
-    // Display fallback UI
+    // display fallback UI
     this.setState({
       hasError: true
     });
+
+    // log the error
+    this.props.logError(
+      this.props.appName,
+      'Error: Component',
+      {
+        error,
+        info,
+      }
+    );
   }
 
   render() {
     if (this.state.hasError) {
-      // You can render any custom fallback UI
-      return <h1>Something went wrong.</h1>;
+      // you can render any custom fallback UI
+      return null;
     }
 
     return <Root />;
   }
 }
+
+export default observe((app) => {
+  const logger = app.get('logger');
+
+  return {
+    appName: app.getName(),
+    logError: (...args) => logger.error(...args)
+  };
+})(Index);
